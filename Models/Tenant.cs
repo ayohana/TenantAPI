230addc60dad57af6f2dc68811221649788a15c5
Adela Darmansyah <adela.yohana@gmail.com>
2020-04-03 10:12:50 -0700

    Initial migration, add validation

--- Models/Tenant.cs
+++ Models/Tenant.cs
@@ -1,7 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TenantAPI.Models
 {
   public class Tenant
   {
-
+    [Key]
+    public int TenantId { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [Phone]
+    public int Phone { get; set; }
+    [Required]
+    public string ApartmentName { get; set; }
+    [Required]
+    [StringLength(5, ErrorMessage="Apartment Number must not exceed 5 characters.")]
+    public string ApartmentNumber { get; set; }
+    public bool IsBackgroundChecked { get; set; }
   }
 }
