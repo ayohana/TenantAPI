e7c90ae2f0428a70daa64e6fc1e3ac4e4884d2e6
Adela Darmansyah <adela.yohana@gmail.com>
2020-04-03 10:46:23 -0700

    Add data seeding

---
+++ Migrations/20200403174508_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TenantAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tenants",
+                columns: new[] { "TenantId", "ApartmentName", "ApartmentNumber", "Email", "FirstName", "IsBackgroundChecked", "LastName", "Phone" },
+                values: new object[,]
+                {
+                    { 1, "Baker Apartments", "221B", "assistant@holmes.com", "John", true, "Watson", "4477009999" },
+                    { 2, "Baker Apartments", "221B", "detective@holmes.com", "Sherlock", true, "Holmes", "4477009999" },
+                    { 3, "Baker Apartments", "221A", "housekeeper@holmes.com", "Mrs", true, "Hudson", "4477008888" },
+                    { 4, "Downing Apartments", "303", "specialist@morgue.com", "Molly", true, "Hooper", "4476001111" },
+                    { 5, "Stonyhurst Place", "V20", "mastermind@villain.com", "James", false, "Moriarty", "4470004444" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tenants",
+                keyColumn: "TenantId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tenants",
+                keyColumn: "TenantId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tenants",
+                keyColumn: "TenantId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tenants",
+                keyColumn: "TenantId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Tenants",
+                keyColumn: "TenantId",
+                keyValue: 5);
+        }
+    }
+}
--- Models/TenantAPIContext.cs
+++ Models/TenantAPIContext.cs
@@ -10,5 +10,17 @@ namespace TenantAPI.Models
     }
 
     public DbSet<Tenant> Tenants { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Tenant>()
+        .HasData(
+          new Tenant { TenantId = 1, FirstName = "John", LastName = "Watson", Email = "assistant@holmes.com", Phone = "4477009999", ApartmentName = "Baker Apartments", ApartmentNumber = "221B", IsBackgroundChecked = true },
+          new Tenant { TenantId = 2, FirstName = "Sherlock", LastName = "Holmes", Email = "detective@holmes.com", Phone = "4477009999", ApartmentName = "Baker Apartments", ApartmentNumber = "221B", IsBackgroundChecked = true },
+          new Tenant { TenantId = 3, FirstName = "Mrs", LastName = "Hudson", Email = "housekeeper@holmes.com", Phone = "4477008888", ApartmentName = "Baker Apartments", ApartmentNumber = "221A", IsBackgroundChecked = true },
+          new Tenant { TenantId = 4, FirstName = "Molly", LastName = "Hooper", Email = "specialist@morgue.com", Phone = "4476001111", ApartmentName = "Downing Apartments", ApartmentNumber = "303", IsBackgroundChecked = true },
+          new Tenant { TenantId = 5, FirstName = "James", LastName = "Moriarty", Email = "mastermind@villain.com", Phone = "4470004444", ApartmentName = "Stonyhurst Place", ApartmentNumber = "V20", IsBackgroundChecked = false }
+        );
+    }
   }
 }
